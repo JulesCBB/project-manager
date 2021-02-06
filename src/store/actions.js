3e5b2c7fbaa9adc759297e6e3c4a2ff78c1d728b
julianacristina.bertobuzanello <julianacristina.bertobuzanello@student.kuleuven.be>
2021-02-06 16:11:54 +0100

    changed data file

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,4 +1,4 @@
-const INITIAL_DATA_URL = "https://raw.githubusercontent.com/JulesCBB/project-manager/master/src/db.json"
+const INITIAL_DATA_URL = "https://raw.githubusercontent.com/techlab23/data-repository/master/boards.json"
 import axios from "axios"
 
 export default {
