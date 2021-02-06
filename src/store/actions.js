802e99255e0b50b9d8973e172ad7822ced53dbbc
julianacristina.bertobuzanello <julianacristina.bertobuzanello@student.kuleuven.be>
2021-02-06 16:06:10 +0100

    added extra action component

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,4 +1,4 @@
-const INITIAL_DATA_URL = "http://localhost:3000"
+const INITIAL_DATA_URL = "https://raw.githubusercontent.com/techlab23/data-repository/master/boards.json"
 import axios from "axios"
 
 export default {
